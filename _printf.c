8bac509c7a44b47c95f9710ea62fc39f000eeaa1
Chijioke Chika <8pluscreative@gmail.com>
2022-08-08 20:53:55 +0100

    Fixed

--- _printf.c
+++ _printf.c
@@ -4,63 +4,63 @@
 
 /**
  * _puts - prints a string
- * _printf - prints anything
+ * _printf - prints any arg
+ * @str: pointer to str
  * @format: the format string
  *
  * Return: the number of characters printed
  */
-
 int _puts(char *str)
 {
 	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	return (i);
+		while (str[i])
+
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		return (i);
 }
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	int i = 0;
+	va_list data;
 
-int _printf(const char *format, ...){
-  int count = 0;
-  int i = 0;
+	va_start(data, format);
+		while (format[i])
+		{
+			while (format[i] == '%')
+			{
+				switch (format[i + 1])
+				{
+					case 'c':
+						count += _putchar(va_arg(data, int));
+						i += 2;
+						break;
+					case 's':
+						count += _putchar(_puts(va_arg(data, char *)));
+						i += 2;
+						break;
+					case '%':
+						_putchar('%');
+						count++;
+						i += 2;
+						break;
+					default:
+						_putchar(format[i]);
+						_putchar(format[i]);
+						i += 2;
+				}
+			}
 
-  va_list data; 
-  va_start(data, format);
-
-  while(format[i]) 
-  {
-	 while(format[i] == '%')
-          {
-                  switch(format[i + 1])
-                  {
-                          case 'c':
-                                  count += _putchar(va_arg(data, int));
-                                  i += 2;
-                                  break;
-			  case 's':
-				  count += _putchar(_puts(va_arg(data, char *)));
-				  i += 2;  
-				  break;
- 			  case '%':
-                                   _putchar('%');
-                                   count++;
-                                   i += 2;
-                                   break; 
-                          default:
-                                 _putchar(format[i]);
-				 _putchar(format[i]);
-			 	i += 2;
-                 }
-          }
-
-  if(format[i])
+	if (format[i])
 	{
 		_putchar(format[i]);
 		count++;
 	}
 	i++;
-  } 
+}
 
-  return(count);
+return (count);
 }
--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg>
-#include <stdlib>
+#include <stdarg.h>
+#include <stdlib.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
